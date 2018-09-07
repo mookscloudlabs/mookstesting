10134bd0ceb1cb8f02702e2ca8479e6599a7b845
Mooks Training <43074902+mookscloudlabs@users.noreply.github.com>
2018-09-07 21:44:05 +0100

    Mooks Testing

--- mooks1stscript.ps1
+++ mooks1stscript.ps1
@@ -4,3 +4,5 @@ $task.Settings.Hidden = $true
 Add-TaskAction -Task $task -Path C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe –Arguments “-File C:\Scripts\GroupMembershipChanges.ps1”
 Add-TaskTrigger -Task $task -Daily -At “10:00”
 Register-ScheduledJob –Name ”Monitor Group Management” -Task $task
+
+# Mooks Testing Adding some more tyest
